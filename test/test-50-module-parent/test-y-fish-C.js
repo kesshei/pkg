e550f635c7975aa5f8ec508fa1270081005c5419
kesshei <dagang520@qq.com>
2026-04-02 10:22:33 +0800

    移除test

--- test/test-50-module-parent/test-y-fish-C.js
+++
@@ -1,5 +0,0 @@
-'use strict';
-
-var path = require('path');
-var mpfn = module.parent.filename;
-module.exports = mpfn.split(path.sep).slice(-2).join(path.sep);
--- test/test-79-npm/natives/natives.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-var natives = require('natives');
-var fsCopy = natives.require('fs');
-if (fsCopy !== require('fs')) {
-  console.log('ok');
-}
